b57ae7c7f54273358b1b8d2dc0df022293a95771
adamfachreza <adamfachreza15@gmail.com>
2023-08-24 14:47:56 +0700

    update chat main profile dan wishlist

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/pages/home/chat_page.dart';
+import 'package:shamo/pages/home/home_page.dart';
+import 'package:shamo/pages/home/profile_page.dart';
+import 'package:shamo/pages/home/wishlist_page.dart';
 import 'package:shamo/theme.dart';
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
 
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     Widget cartButton() {
@@ -26,33 +34,57 @@ class MainPage extends StatelessWidget {
           clipBehavior: Clip.antiAlias,
           child: BottomNavigationBar(
             backgroundColor: backgroundColor4,
+            currentIndex: currentIndex,
+            onTap: (value) {
+              print(value);
+
+              setState(() {
+                currentIndex = value;
+              });
+            },
             type: BottomNavigationBarType.fixed,
             items: [
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/icon_home.png',
-                  width: 21,
+                icon: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Image.asset(
+                    'assets/icon_home.png',
+                    width: 21,
+                    color: currentIndex == 0 ? primaryColor : primaryTextColor,
+                  ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/icon_chat.png',
-                  width: 21,
+                icon: Container(
+                  margin: EdgeInsets.only(top: 20, right: 50),
+                  child: Image.asset(
+                    'assets/icon_chat.png',
+                    width: 21,
+                    color: currentIndex == 1 ? primaryColor : primaryTextColor,
+                  ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/icon_wishlist.png',
-                  width: 21,
+                icon: Container(
+                  margin: EdgeInsets.only(top: 20, left: 50),
+                  child: Image.asset(
+                    'assets/icon_wishlist.png',
+                    width: 21,
+                    color: currentIndex == 2 ? primaryColor : primaryTextColor,
+                  ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/icon_profile.png',
-                  width: 21,
+                icon: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Image.asset(
+                    'assets/icon_profile.png',
+                    width: 21,
+                    color: currentIndex == 3 ? primaryColor : primaryTextColor,
+                  ),
                 ),
                 label: '',
               ),
@@ -62,11 +94,32 @@ class MainPage extends StatelessWidget {
       );
     }
 
+    Widget body() {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+          break;
+
+        case 1:
+          return ChatPage();
+          break;
+        case 2:
+          return WishlistPage();
+          break;
+        case 3:
+          return ProfilePage();
+          break;
+        default:
+          throw Exception("Invalid index: $currentIndex");
+      }
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor1,
       floatingActionButton: cartButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: customButtomNav(),
+      body: body(),
     );
   }
 }
